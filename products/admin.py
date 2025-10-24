48337aaa848a1a2c20ae80bdef95d98fc7257b85
pabrod17 <pabrod17@gmail.com>
2025-10-24 03:42:41 +0200

    Added new offers table in the admin panel. Create columns for offers and products admin tables

--- products/admin.py
+++ products/admin.py
@@ -1,4 +1,15 @@
+from os import name
 from django.contrib import admin
-from .models import Product
+from .models import Offer, Product
 
-admin.site.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock')
+
+
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ('code', 'discount')
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Offer, OfferAdmin)
