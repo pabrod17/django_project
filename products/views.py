a20d1bab032436c6610e675034fd6a0bfa231b82
pabrod17 <pabrod17@gmail.com>
2025-10-24 03:52:28 +0200

    Showing list of static products

--- products/views.py
+++ products/views.py
@@ -1,12 +1,17 @@
+import re
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Product
 
 # Create your views here.
 # /products -> index
 # Uniform Resource Locator (Address)
 def index(request):
-    return HttpResponse('Hello World')
+    Product.objects.all()
+    # Product.objects.filter()
+    # Product.objects.save()
+    # return HttpResponse('Hello World')
+    return render(request, 'index.html')
 
 
 def new(request):
